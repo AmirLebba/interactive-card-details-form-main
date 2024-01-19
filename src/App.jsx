eb716e65e921a15b45181bcace8dad2dbcffae3f
Amir <amirlebba@gmail.com>
2024-01-19 23:03:33 +0200

    emprove the form validation

--- src/App.jsx
+++ src/App.jsx
@@ -1,36 +1,41 @@
 import { useState } from 'react'
+import tick from './Assets/icon-complete.svg'
 
 
 function App() {
-  const [cardHolder,setCardHolder] = useState("")
+  const [cardHolder, setCardHolder] = useState("");
   const [cardNumb, setCardNumb] = useState("");
   const [expDay, setExpDay] = useState("");
   const [expMonth, setExpMonth] = useState("");
   const [cvc, setCvc] = useState("");
-
-  const handleCardHolderChange =(e) =>{
+  const [confirm, setConfirm] = useState(false);
+  const [formValid, setFormValid] = useState(false);
+  const handleCardHolder = (e) => {
     setCardHolder(e.target.value);
-    
-  }
-  const handleCardNumbChange = (e) => {
-    
+  };
+  const handleCardNumb = (e) => {
     setCardNumb(e.target.value);
-    
   };
-  const handleExpDayChange = (e) => {
-   
+  const handleExpDay = (e) => {
     setExpDay(e.target.value);
-   
   };
-  const handleExpMonthChange = (e) => {
-    
+  const handleExpMonth = (e) => {
     setExpMonth(e.target.value);
-    
   };
-  const handleCvcChange = (e) => {
-    
+  const handleCvc = (e) => {
     setCvc(e.target.value);
   };
+  const hadleClick = (e) => {
+    setConfirm(!confirm);
+  };
+  
+  /**
+   * Renders the credit card form with inputs for
+   * cardholder name, card number, expiration date,
+   * and CVC code. Uses React state to store values.
+   * Displays preview of card details. Handles input
+   * changes to update state.
+   */
   return (
     <>
       <aside className="side-bar"></aside>
@@ -56,64 +61,90 @@ function App() {
           {expDay}/{expMonth}
         </p>
       </div>
-      <div className="form">
-        <p>CARDHOLDER NAME</p>
-        <input
-          type="text"
-          value={cardHolder}
-          onChange={handleCardHolderChange}
-          name=""
-          id=""
-          placeholder="e.g.Jane Appleseed"
-        />
-        <p>CARD NUMBER</p>
-        <input
-          type="number"
-          value={cardNumb}
-          id="CardHolder"
-          placeholder="e.g. 1234 5678 9123 0000"
-          maxLength={16}
-          onChange={handleCardNumbChange}
-        />
-        <div className="exp_cvc_holder">
-          <div className="exp">
-            <p>EXP.DATE(MM/YY)</p>
-            <input
-              type="number"
-              name=""
-              id=""
-              maxLength={2}
-              placeholder="MM"
-              onChange={handleExpDayChange}
-              value={expDay}
-            />
-            <input
-              type="number"
-              name=""
-              id=""
-              maxLength={2}
-              placeholder="YY"
-              onChange={handleExpMonthChange}
-              value={expMonth}
-            />
+      {!confirm && (
+        <form  className="form">
+          <label htmlFor='cardHolder'>CARDHOLDER NAME</label>
+          <input
+            type="text"
+            value={cardHolder}
+            onChange={handleCardHolder}
+            name="cardHolder"
+            id=""
+            placeholder="e.g.Jane Appleseed"
+            required
+          />
+          <label htmlFor='cardNumb'>CARD NUMBER</label>
+          <input
+            type="text"
+            value={cardNumb
+              .replace(/\s/g, "")
+              .replace(/(\d{4})/g, "$1 ")
+              .trim()}
+            name='cardNumb'
+            placeholder="e.g. 1234 5678 9123 0000"
+            maxLength={19}
+            onChange={handleCardNumb}
+            required
+          />
+          
+          <div className="exp_cvc_holder">
+            <div className="exp">
+              <label htmlFor='MM'>EXP.DATE(MM/YY)</label>
+              <input
+                type="text"
+                name="MM"
+                id="Month"
+                maxLength={2}
+                placeholder="MM"
+                onChange={handleExpDay}
+                value={expDay}
+              />
+              <input
+                type="text"
+                name="YY"
+                id="Year"
+                maxLength={2}
+                placeholder="YY"
+                onChange={handleExpMonth}
+                value={expMonth}
+              />
+            </div>
+            <div className="cvc">
+              <label htmlFor='CVC'>CVC</label>
+              <input
+                type="text"
+                name="CVC"
+                id="CVC"
+                maxLength={3}
+                placeholder="e.g. 123"
+                onChange={handleCvc}
+                value={cvc}
+              />
+              
+            </div>
           </div>
-          <div className="cvc">
-            <p>CVC</p>
-            <input
-              type="number"
-              name=""
-              id=""
-              maxLength={3}
-              placeholder="e.g. 123"
-              onChange={handleCvcChange}
-              value={cvc}
-            />
-          </div>
-        </div>
-        <button>Confirm</button>
-      </div>
+          <button onClick={hadleClick}>Confirm</button>
+        </form>
+      )}
+      {confirm && <ThankYou setConfirm={setConfirm} />}
     </>
   );
 }
 
 export default App
+
+
+/*confirmation complite app*/
+
+function ThankYou({ setConfirm }) {
+  return (
+    <>
+      <div className="confirmation">
+        <img src={tick} alt="" />
+        <h1>Thank you!</h1>
+        <p>We've added your card details</p>
+        <button onClick={() => setConfirm(false)}>Continue</button>
+      </div>
+    </>
+  );
+}
