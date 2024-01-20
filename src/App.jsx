224aa617379c4a14ea555782892f39a89face74d
Amir <amirlebba@gmail.com>
2024-01-20 23:32:08 +0200

    adjusting the error condition redndering

--- src/App.jsx
+++ src/App.jsx
@@ -1,46 +1,63 @@
-import { useState } from 'react'
-import tick from './Assets/icon-complete.svg'
-
+import { useState, useEffect } from "react";
+import tick from "./Assets/icon-complete.svg";
 
 function App() {
-  const [cardHolder, setCardHolder] = useState("");
-  const [cardNumb, setCardNumb] = useState("");
-  const [expDay, setExpDay] = useState("");
-  const [expMonth, setExpMonth] = useState("");
-  const [cvc, setCvc] = useState("");
-  const [confirm, setConfirm] = useState(false);
-  const [formValid, setFormValid] = useState(false);
-  const handleCardHolder = (e) => {
-    setCardHolder(e.target.value);
-  };
-  const handleCardNumb = (e) => {
-    setCardNumb(e.target.value);
-  };
-  const handleExpDay = (e) => {
-    setExpDay(e.target.value);
-  };
-  const handleExpMonth = (e) => {
-    setExpMonth(e.target.value);
-  };
-  const handleCvc = (e) => {
-    setCvc(e.target.value);
-  };
-  const hadleClick = (e) => {
-    setConfirm(!confirm);
-  };
+  const [formData, setFormData] = useState({
+    cardHolder: "",
+    cardNumb: "",
+    expMonth: "",
+    expYear: "",
+    cvc:"",
+  });
   
-  /**
-   * Renders the credit card form with inputs for
-   * cardholder name, card number, expiration date,
-   * and CVC code. Uses React state to store values.
-   * Displays preview of card details. Handles input
-   * changes to update state.
-   */
+  const [validationErrors, setValidationErrors] = useState({});
+  const [formValid, setFormValid] = useState(false);
+  const handleChange = (e) => {
+    const { name, value } = e.target;
+    setFormData((prevFormData) => ({
+      ...prevFormData,
+      [name]: value,
+    }));
+  };
+  const handleFormSubmit = () => {
+    const validationErrors = {};
+
+    if (!formData.cardHolder.trim()) {
+      validationErrors.cardHolder = "Name can't be blank";
+    } else if (!/^[a-zA-Z ]+$/.test(formData.cardHolder)) {
+      validationErrors.cardHolder = "Name is not valid";
+    }
+
+    const cardNumbWithoutSpaces = formData.cardNumb.replace(/\s/g, "");
+    if (!cardNumbWithoutSpaces.trim()) {
+      validationErrors.cardNumb = "Card number is required";
+    } else if (
+      !/^\d+$/.test(cardNumbWithoutSpaces) ||
+      cardNumbWithoutSpaces.length !== 16
+    ) {
+      validationErrors.cardNumb = "Card number is not valid";
+    }
+
+    if (!formData.expMonth.trim()) {
+      validationErrors.expMonth = <span>can't be blank</span>;
+    }
+
+    if (!formData.expYear.trim()) {
+      validationErrors.expYear = <span>can't be blank</span>;
+    }
+
+    if (!formData.cvc.trim()) {
+      validationErrors.cvc = "CVC can't be blank";
+    }
+
+    return validationErrors;
+  };
+
   return (
     <>
       <aside className="side-bar"></aside>
       <div className="back-card">
-        <p>{cvc}</p>
+        <p>{formData.cvc}</p>
       </div>
       <div className="front-card">
         <svg
@@ -55,95 +72,109 @@ function App() {
             stroke="#fff"
           />
         </svg>
-        <h1>{cardNumb}</h1>
-        <p id="name">{cardHolder}</p>
+        <h1>{formData.cardNumb}</h1>
+        <p id="name">{formData.cardHolder}</p>
         <p id="date">
-          {expDay}/{expMonth}
+          {formData.expMonth}/{formData.expYear}
         </p>
       </div>
-      {!confirm && (
-        <form  className="form">
-          <label htmlFor='cardHolder'>CARDHOLDER NAME</label>
+      {!formValid && (
+        <form className="form" onSubmit={handleFormSubmit}>
+          <label htmlFor="cardHolder">CARDHOLDER NAME</label>
           <input
             type="text"
-            value={cardHolder}
-            onChange={handleCardHolder}
+            value={formData.cardHolder}
+            onChange={handleChange}
             name="cardHolder"
-            id=""
+            id="cardHolder"
             placeholder="e.g.Jane Appleseed"
             required
           />
-          <label htmlFor='cardNumb'>CARD NUMBER</label>
+          {validationErrors.cardHolder && (
+            <span>{validationErrors.cardHolder}</span>
+          )}
+          <label htmlFor="cardNumb">CARD NUMBER</label>
           <input
             type="text"
-            value={cardNumb
+            value={formData.cardNumb
               .replace(/\s/g, "")
               .replace(/(\d{4})/g, "$1 ")
               .trim()}
-            name='cardNumb'
+            name="cardNumb"
+            id="cardNumb"
             placeholder="e.g. 1234 5678 9123 0000"
             maxLength={19}
-            onChange={handleCardNumb}
+            onChange={handleChange}
             required
           />
-          
+          {validationErrors.cardNumb && (
+            <span>{validationErrors.cardNumb}</span>
+          )}
+
           <div className="exp_cvc_holder">
             <div className="exp">
-              <label htmlFor='MM'>EXP.DATE(MM/YY)</label>
+              <label htmlFor="MM">EXP.DATE(MM/YY)</label>
               <input
                 type="text"
-                name="MM"
-                id="Month"
+                name="expMonth"
+                id="MM"
                 maxLength={2}
                 placeholder="MM"
-                onChange={handleExpDay}
-                value={expDay}
+                onChange={handleChange}
+                value={formData.expMonth}
+                required
               />
+
               <input
                 type="text"
-                name="YY"
-                id="Year"
+                name="expYear"
+                id="YY"
                 maxLength={2}
                 placeholder="YY"
-                onChange={handleExpMonth}
-                value={expMonth}
+                onChange={handleChange}
+                value={formData.expYear}
+                required
               />
+              {validationErrors.expMonth ||
+                (validationErrors.expYear && (
+                  <span>{validationErrors.expYear}</span>
+                ))}
             </div>
             <div className="cvc">
-              <label htmlFor='CVC'>CVC</label>
+              <label htmlFor="CVC">CVC</label>
               <input
                 type="text"
-                name="CVC"
+                name="cvc"
                 id="CVC"
                 maxLength={3}
                 placeholder="e.g. 123"
-                onChange={handleCvc}
-                value={cvc}
+                onChange={handleChange}
+                value={formData.cvc}
+                required
               />
-              
+              {validationErrors.cvc && <span>{validationErrors.cvc}</span>}
             </div>
           </div>
-          <button onClick={hadleClick}>Confirm</button>
+          <button type="submit">Confirm</button>
         </form>
       )}
-      {confirm && <ThankYou setConfirm={setConfirm} />}
+      {formValid && <ThankYou setConfirm={setFormValid} />}
     </>
   );
 }
 
-export default App
-
+export default App;
 
 /*confirmation complite app*/
 
-function ThankYou({ setConfirm }) {
+function ThankYou({ setFormValid }) {
   return (
     <>
       <div className="confirmation">
         <img src={tick} alt="" />
         <h1>Thank you!</h1>
         <p>We've added your card details</p>
-        <button onClick={() => setConfirm(false)}>Continue</button>
+        <button onClick={() => setFormValid(false)}>Continue</button>
       </div>
     </>
   );
